3036b8484e936fdaeaedc9c5f10e0c849725cca0
GET Academy <terje@kolderup.net>
2020-01-17 14:28:20 +0100

    Initial commit

---
+++ FilesAndFolders/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilesAndFolders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
